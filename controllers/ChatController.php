d32f4af3abfd4ab683da9d16f7fd0b9904e48aae
VIRTHS12 <187938754+VIRTHS12@users.noreply.github.com.>
2026-07-07 03:19:32 +0700

    fix pliss3

--- controllers/ChatController.php
+++ controllers/ChatController.php
@@ -333,7 +333,7 @@ class ChatController
                         "mode"     => "backend"
                     ],
                     "role"         => "operator",
-                    "scopes" => ["operator.pairing, operator.read", "operator.write"],
+                    "scopes" => ["operator.admin", "operator.read", "operator.write"],
                     "auth"         => [
                         "token" => $token
                     ]
